f71772173c1de6249a57a466d1903da205fb01c2
XavierR007 <christianxavier117@outlook.com>
2023-03-16 15:47:44 -0600

    agregar proyecto

---
+++ ejercicio2/ejercicio2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool band=true;
+            int num = int.Parse(Console.ReadLine());
+            
+            for (int n=2; n<num; n++)
+            {
+                if (num % n == 0)
+                {
+                    band = false;
+                }
+            }
+            if (band)
+            {
+                Console.WriteLine("es primo");
+            }
+
+            
+            }
+        }
+    }
